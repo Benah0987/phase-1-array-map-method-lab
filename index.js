3a79e95176f28dc243636e2b35998f7f8b7c9d3d
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-12 07:07:49 +0300

    added a function

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    "what is the constructor oo pattern?",
+    "implementing blockchain web api",
+    "the test driven development workflow",
+    "what is NaN and how can we check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is jsonp?"
+  ];
 
-const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    let words = tutorial.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  });
 }
